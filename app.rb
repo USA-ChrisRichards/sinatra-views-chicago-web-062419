d874d6abe08d980715977abef5675011c9e2649e
github username <github email address>
2019-07-15 11:27:36 -0500

    done

--- app.rb
+++ app.rb
@@ -2,7 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-	get '/' do
-		
+	get '/' do 
+		erb :index
+	end
+	#* This tells Sinatra to render a file called `index.erb` inside of a directory called `views`.
+	#create a route called "/info" in our controller
+	get "/info" do
+		erb :info
 	end
 end
